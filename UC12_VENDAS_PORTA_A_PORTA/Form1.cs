5a8f499237d3fd343a96fc4e40a1832b804b7c9d
MIGUEL DE PAULA VELOSO <miguel.pveloso@senacsp.edu.br>
2023-05-26 19:42:40 -0300

    Adicionar arquivos de projeto.

---
+++ UC12_VENDAS_PORTA_A_PORTA/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UC12_VENDAS_PORTA_A_PORTA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ UC12_VENDAS_PORTA_A_PORTA/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UC12_VENDAS_PORTA_A_PORTA
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
